8e4e8eed9283563d25c1c18016d005b649ad9ed2
Pedro Henrique <seuemail@example.com>
2025-10-19 13:50:09 -0300

    feat: Adiciona Mistral 7B GGUF local e ajusta o utils.py para deploy no Render

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import threading
 
 
 # Inicializa o modelo
-model = GPT4All("mistral-7b-instruct-v0.2.Q4_0.gguf")
+#model = GPT4All("mistral-7b-instruct-v0.2.Q4_0.gguf")
 
 
 # --- Funções que analisam os investimentos separadamente (Mantidas) ---
